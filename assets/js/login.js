e970a0e1fe99119997a3b93d0d79ebdf0336effc
YuYiHao <1293937984@qq.com>
2021-01-10 21:01:56 +0800

    修改login.html

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,60 @@
 $(function() {
     //点击去注册账号链接
-    $('#link-login').on('click', function() {
+    $('#link-login').on('click contextmenu selectstart', function(e) {
         $(".box-login").hide();
         $(".box-reg").show();
+        e.preventDefault();
     });
     //点击去登陆链接
-    $('#link-reg').on('click', function() {
+    $('#link-reg').on('click contextmenu selectstart', function(e) {
         $(".box-login").show();
         $(".box-reg").hide();
-    })
+        e.preventDefault();
+    });
+    //从layui中获取form对象
+    var form = layui.form;
+    // 通过form.verify()自定义校验规则
+    form.verify({
+        username: function(value, item) { //value：表单的值、item：表单的DOM对象
+            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+                return '用户名不能有特殊字符';
+            }
+            if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+                return '用户名首尾不能出现下划线\'_\'';
+            }
+            if (/^\d+\d+\d$/.test(value)) {
+                return '用户名不能全为数字';
+            }
 
+            //如果不想自动弹出默认提示框，可以直接返回 true，这时你可以通过其他任意方式提示（v2.5.7 新增）
+            if (value === '啊') {
+                // alert('用户名不能为敏感词');
+                return '用户名不能为敏感词';
+            }
+        },
+        //我们既支持上述函数式的方式，也支持下述数组的形式
+        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+
+        pass: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        repass: function(value) {
+            var pwd = $('.box-reg [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+
+        }
+    });
+    // 监听注册表单的提交事件
+    $("#form_reg").on('submit', function(e) {
+        e.preventDefault()
+        $.post('http://www.liulongbin.top:3007/api/reguser', { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }, function(res) {
+            if (res.status !== 0) {
+                return console.log(res.message);
+            }
+            console.log('注册成功');
+        })
+
+    })
 })
