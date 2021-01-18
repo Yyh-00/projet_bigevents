d0a5fbf2ef21f3186fc90f8cd95083a5d59cba67
YuYiHao <1293937984@qq.com>
2021-01-18 13:16:00 +0800

    xiugai

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,77 +1,77 @@
- $(function() {
-     //调用获去用户信息
-     getUserInfo();
-     //实现退出功能
-     $("#btnLoginout").on("click", function() {
-         layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function(index) {
-             //清空本地存储内容
-             localStorage.removeItem('token');
-             //跳转到登陆界面
-             location.href = "/login.html";
-             disableBack();
-             //关闭comfirm询问框
-             layer.close(index);
-         });
-     });
+$(function() {
+    //调用获去用户信息
+    getUserInfo();
+    //实现退出功能
+    $("#btnLoginout").on("click", function() {
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function(index) {
+            //清空本地存储内容
+            localStorage.removeItem('token');
+            //跳转到登陆界面
+            location.href = "/login.html";
+            disableBack();
+            //关闭comfirm询问框
+            layer.close(index);
+        });
+    });
 
-     //封装获取用户信息函数
-     function getUserInfo() {
-         $.ajax({
-             url: '/my/userinfo',
-             method: 'get',
-             //headers就是请求头配置对象
-             /* headers: {
-                 Authorization: localStorage.getItem('token')
-             }, */
-             success: function(res) {
-                 if (res.status !== 0) {
-                     return layui.layer.msg('获取用户信息失败！')
+    //封装获取用户信息函数
+    function getUserInfo() {
+        $.ajax({
+            url: '/my/userinfo',
+            method: 'get',
+            //headers就是请求头配置对象
+            /* headers: {
+                Authorization: localStorage.getItem('token')
+            }, */
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败！')
+                }
+                //调用渲染用户头像函数
+                renderAvatar(res.data);
+            },
+            //不论请求成功还是失败，都会调用complete回调函数
+            /*  complete: function(res) {
+                 console.log(res);
+                 if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                     //强制清除本地存储的内容
+                     localStorage.removeItem("token");
+                     //强制跳转到登陆界面
+                     location.href = '/login.html';
                  }
-                 //调用渲染用户头像函数
-                 renderAvatar(res.data);
-             },
-             //不论请求成功还是失败，都会调用complete回调函数
-             /*  complete: function(res) {
-                  console.log(res);
-                  if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-                      //强制清除本地存储的内容
-                      localStorage.removeItem("token");
-                      //强制跳转到登陆界面
-                      location.href = '/login.html';
-                  }
-              } */
+             } */
 
-         })
-     }
+        })
+    }
 
-     //渲染用户头像
-     function renderAvatar(user) {
-         //1.获取用户名称
-         let name = user.nickname || user.username;
-         //2.设置欢迎的文本
-         $("#welcome").html("欢迎&nbsp&nbsp;" + name);
-         //3.按需渲染用户头像
-         if (user.user_pic !== null) {
-             $(".layui-nav-img").attr("src", user.user_pic).show();
-             $(".text-avatar").hide();
-         } else {
-             $(".layui-nav-img").hide();
-             let first = name[0].toUpperCase();
-             $(".text-avatar").html(first).show();
+    //渲染用户头像
+    function renderAvatar(user) {
+        //1.获取用户名称
+        let name = user.nickname || user.username;
+        //2.设置欢迎的文本
+        $("#welcome").html("欢迎&nbsp&nbsp;" + name);
+        //3.按需渲染用户头像
+        if (user.user_pic !== null) {
+            $(".layui-nav-img").attr("src", user.user_pic).show();
+            $(".text-avatar").hide();
+        } else {
+            $(".layui-nav-img").hide();
+            let first = name[0].toUpperCase();
+            $(".text-avatar").html(first).show();
 
-         }
+        }
 
-     };
+    };
 
-     //禁用浏览器后退按钮
-     function disableBack() {
-         if (window.history && window.history.pushState) {
-             $(window).on('popstate', function() {
-                 window.history.pushState('forward', null, '');
-                 window.history.forward(1);
-             });
-         }
-         window.history.pushState('forward', null, '');
-         window.history.forward(1);
-     }
- })
+    //禁用浏览器后退按钮
+    function disableBack() {
+        if (window.history && window.history.pushState) {
+            $(window).on('popstate', function() {
+                window.history.pushState('forward', null, '');
+                window.history.forward(1);
+            });
+        }
+        window.history.pushState('forward', null, '');
+        window.history.forward(1);
+    }
+})
---
+++ assets/js/user/user_pwd.js
@@ -0,0 +1,40 @@
+$(function() {
+    var form = layui.form;
+    var layer = layui.layer;
+
+    //重置密码表单验证
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        samePwd: function(value) {
+            if (value === $('[name=pwd]').val()) {
+                return '新旧密码不能相同！'
+            }
+        },
+        confirmPwd: function(value) {
+            if (value !== $('[name=newPwd]').val()) {
+                return '两次密码不一致！'
+            }
+        }
+    });
+
+
+    //绑定表单的提交事件
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('更新密码失败！')
+                }
+                layui.layer.msg('更新密码成功！');
+                // 重置表单
+                $('.layui-form')[0].reset()
+            }
+        })
+    })
+})
